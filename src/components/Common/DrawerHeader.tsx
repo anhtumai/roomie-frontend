b36c081dd36e055d03f143f6bbba76ab13738ebc
anhtumai <tu.mai@outlook.com>
2021-11-21 19:59:39 +0200

    ignore DrawerHeader to avoid typescript error

--- src/components/Common/DrawerHeader.tsx
+++ src/components/Common/DrawerHeader.tsx
@@ -1,13 +1,13 @@
 // @ts-ignore
 import { styled } from "@mui/material/styles";
 
-const DrawerHeader = styled("div")(({ theme }) => ({
+const DrawerHeader = styled("div")((data: any) => ({
   display: "flex",
   alignItems: "center",
   justifyContent: "flex-end",
-  padding: theme.spacing(0, 1),
+  padding: data.theme.spacing(0, 1),
   // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
+  ...data.theme.mixins.toolbar,
 }));
 
 export default DrawerHeader;
