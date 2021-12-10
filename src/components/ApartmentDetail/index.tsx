ba74a34e8f6781b394c6215be156823a1b04e670
anhtumai <tu.mai@outlook.com>
2021-12-10 02:48:24 +0200

    style, make width of apartment detail page fit mobile screen

--- src/components/ApartmentDetail/index.tsx
+++ src/components/ApartmentDetail/index.tsx
@@ -49,50 +49,49 @@ function ApartmentDetail({ apartment }: { apartment: Apartment }) {
 
   return (
     <div className="apartment-detail">
-      <div
-        style={{
-          marginLeft: "1.5rem",
-        }}
-      >
-        <div className="apartment-detail__header">
-          <h2>{apartment.name}</h2>
-          {isAdmin && (
-            <IconButton onClick={handleRename}>
-              <EditIcon htmlColor="#505f78" />
-              <span>Rename</span>
-            </IconButton>
-          )}
-          <IconButton onClick={() => history.goBack()}>
-            <KeyboardReturnIcon htmlColor="#505f78" />
-            <span>Go back</span>
+      <div className="apartment-detail__header">
+        <div
+          style={{
+            flexGrow: 1,
+          }}
+        ></div>
+        {isAdmin && (
+          <IconButton onClick={handleRename}>
+            <EditIcon htmlColor="#505f78" />
+            <span>Rename</span>
           </IconButton>
-        </div>
-        <div>
-          <h4>Admin</h4>
-          <MemberDisplay member={apartment.admin} />
-          <h4>Normal Members</h4>
-          {apartment.members
-            .filter((member) => member.id !== apartment.admin.id)
-            .map((member) => (
-              <div
-                key={member.id}
-                className="apartment-detail__member-display-wrapper"
-              >
-                <div className="apartment-detail__member-display">
-                  <MemberDisplay member={member} />
-                </div>
-                {isAdmin && (
-                  <Button
-                    size="small"
-                    color="error"
-                    onClick={() => handleRemoveMember(member)}
-                  >
-                    Remove
-                  </Button>
-                )}
+        )}
+        <IconButton onClick={() => history.goBack()}>
+          <KeyboardReturnIcon htmlColor="#505f78" />
+          <span>Go back</span>
+        </IconButton>
+      </div>
+      <h2>{apartment.name}</h2>
+      <div>
+        <h4>Admin</h4>
+        <MemberDisplay member={apartment.admin} />
+        <h4>Normal Members</h4>
+        {apartment.members
+          .filter((member) => member.id !== apartment.admin.id)
+          .map((member) => (
+            <div
+              key={member.id}
+              className="apartment-detail__member-display-wrapper"
+            >
+              <div className="apartment-detail__member-display">
+                <MemberDisplay member={member} />
               </div>
-            ))}
-        </div>
+              {isAdmin && (
+                <Button
+                  size="small"
+                  color="error"
+                  onClick={() => handleRemoveMember(member)}
+                >
+                  Remove
+                </Button>
+              )}
+            </div>
+          ))}
       </div>
       <RenameDialog open={openRenameDialog} setOpen={setOpenRenameDialog} />
     </div>
